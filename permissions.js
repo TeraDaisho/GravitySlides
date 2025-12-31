ac4cce388d5da8775e506f7c5f7501f6e6d88cba
TeraDaisho <nakamura@daisho.ac.jp>
2025-12-31 20:39:34 +0900

    chore: Remove unused permission files

--- permissions.js
+++
@@ -1,9 +0,0 @@
-document.getElementById('authBtn').addEventListener('click', async () => {
-    try {
-        await navigator.mediaDevices.getUserMedia({ audio: true });
-        document.getElementById('successMsg').style.display = 'block';
-        document.getElementById('authBtn').style.display = 'none';
-    } catch (err) {
-        alert("Permission denied or error: " + err.message);
-    }
-});
